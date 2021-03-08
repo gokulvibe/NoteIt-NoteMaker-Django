4853760f7c8a44c01c157ae38fe9c00a2dcd0e0b
Gokul Raam <gokulraamofficial@gmail.com>
2021-03-08 20:21:44 +0530

    Added Create New Note Functionality

--- mainnotes/views.py
+++ mainnotes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Notes, Image
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -10,6 +11,24 @@ def mainnotes(request):
     else:
         return redirect('login')
     
-    
+@login_required(redirect_field_name='login')
 def addNote(request):
-    pass
+    if request.method == "POST":
+        title = request.POST['title']
+        description = request.POST['description']
+        print("Printing title:",title)
+        owner = request.user
+        
+        if 'hide_note' in request.POST:
+            hide_note = True
+        else:
+            hide_note = False
+        
+        print("Printing Hide:", hide_note)
+        note = Notes(owner=owner, title=title, description=description, hidden_note = hide_note)
+        note.save()
+        
+        return render(request, 'mainnotes/editnote.html', context={'note':note})
+    
+    else:
+        return render(request, "mainnotes/addnote.html")

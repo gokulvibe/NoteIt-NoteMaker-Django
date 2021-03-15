3cc8ee99519a5b37a97898d47cab0985060c54a1
Gokul Raam <gokulraamofficial@gmail.com>
2021-03-15 19:40:55 +0530

    Added many new features - hide,trash notes, empty trash. Modified some older features.

--- mainnotes/urls.py
+++ mainnotes/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('', views.mainnotes, name='mainnotes'),
     path('addnote', views.addNote, name='addnote'),
     path('editnote/<int:pk>/', views.editnote, name='editnote'),
+    path('trashnote/<int:pk>/', views.trashnote, name='trashnote'),
+    path('hidenote/<int:pk>/', views.hidenote, name='hidenote'),
+    path('hiddennotes', views.hidden_notes, name='hiddennotes'),
+    path('trashednotes', views.trashed_notes, name='trashdnotes'),
+    path('emptytrash', views.empty_trash, name='empty_trash'),
 ]
--- mainnotes/views.py
+++ mainnotes/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
-from .models import Notes, Image
+from .models import Notes, Image, HiddenNotePassword
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 # Create your views here.
 
 
 def mainnotes(request):
     if request.user.is_authenticated:
-        allNotes = Notes.objects.filter(owner = request.user)
+        allNotes = Notes.objects.filter(owner = request.user, hidden_note = False, trashed_note = False)
         return render(request, 'mainnotes/mainnotes.html', context = {'all_notes': allNotes})
     else:
         return redirect('login')
@@ -42,21 +43,17 @@ def addNote(request):
     
     else:
         return render(request, "mainnotes/addnote.html")
-    
+   
+@login_required(redirect_field_name='login') 
 def editnote(request, pk):
     if request.method == 'POST':
         title = request.POST['title']
         print("Printing title:", title)
         description = request.POST['description']
-        if 'hide_note' in request.POST:
-            hide_note = True
-        else:
-            hide_note = False
             
         note = Notes.objects.get(pk=pk)
         note.title = title
         note.description = description
-        note.hidden_note = hide_note
         note.save()
         
         return redirect('/editnote/'+str(note.pk))
@@ -65,3 +62,69 @@ def editnote(request, pk):
         note = Notes.objects.get(pk = pk)
         images = Image.objects.filter(note = note)
         return render(request, 'mainnotes/editnote.html', context={'note':note, 'images':images})
+
+
+
+@login_required(redirect_field_name='login')
+def empty_trash(request):
+    notes = Notes.objects.filter(owner=request.user, trashed_note=True)
+    notes.delete()
+    messages.info(request,'Trash emptied successfully!')
+    return redirect('/trashednotes')
+    
+        
+@login_required(redirect_field_name='login')
+def trashnote(request, pk):
+    note = Notes.objects.get(pk = pk)
+    if note.trashed_note == False:
+        note.trashed_note = True
+        note.save()
+        messages.info(request,"Note Trashed Successfully")
+        return redirect('/')
+    
+    else:
+        note.trashed_note = False
+        note.save()
+        messages.info(request,"Note Restored Successfully")
+        return redirect('/trashednotes')
+
+@login_required(redirect_field_name='login')
+def hidenote(request, pk):
+    note = Notes.objects.get(pk = pk)
+    if note.hidden_note == False:
+        note.hidden_note = True
+        note.save()
+        messages.info(request,"Note Hided Successfully")
+        return redirect('/')
+    
+    else:
+        note.hidden_note = False
+        note.save()
+        messages.info(request,"Note Unhided Successfully")
+        return redirect('/hiddennotes')
+    
+
+@login_required(redirect_field_name='login')
+def hidden_notes(request):
+    if HiddenNotePassword.objects.filter(user=request.user).exists():
+        if request.method == 'POST':
+            passcode = request.POST['password']
+            if HiddenNotePassword.objects.get(user=request.user).hidden_note_password == passcode:
+                allNotes = Notes.objects.filter(owner = request.user, hidden_note = True, trashed_note = False)
+                return render(request, 'mainnotes/hiddennotes.html', context = {'all_notes': allNotes})
+            else:
+                messages.info(request, 'Invalid passcode for hidden notes')
+                return redirect('enterpass')
+            
+        else:
+            return render(request, 'mainnotes/enterhiddenpass.html')
+        
+    else:
+        return render(request, 'mainnotes/createhiddenpass.html')
+
+def trashed_notes(request):
+    allNotes = Notes.objects.filter(owner = request.user, trashed_note = True)
+    return render(request, 'mainnotes/trashednotes.html', context = {'all_notes': allNotes})
+
+
+    

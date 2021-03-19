96eacbd18d33f1f5905cdff1623044b75bf7c37b
Gokul Raam <gokulraamofficial@gmail.com>
2021-03-19 12:19:57 +0530

    Added add friend functionality

--- friends/models.py
+++ friends/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Friend(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    userFriend = models.ForeignKey(User, on_delete=models.CASCADE, name="userfriend")
+    request_accepted = models.BooleanField(default=False)
+    
+class Notification(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    opened = models.BooleanField(default=True)
--- friends/views.py
+++ friends/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from .models import Friend, Notification
 # Create your views here.
+
+def addfriend(request):
+    if request.method=="POST":
+        friend_id = request.POST['friend']
+        friend = User.objects.get(pk = friend_id)
+        friend_object = Friend(user=request.user, userFriend = friend)
+        friend_object.save()
+    try:
+        searchable = request.GET['search']
+    except:
+        searchable = ''
+        
+    user_results = User.objects.filter(email__contains = searchable)
+    
+    return render(request, 'friends/addfriend.html', context={'user_results': user_results})
+
+def viewfriends(request):
+    pass
+    
